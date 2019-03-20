401b67adc472c33a9d3c98638f663f20480a69ba
Tim Lee <tlee87@gmail.com>
2019-03-20 00:03:33 -0700

    working example with file xfer from serever to client. no datagram recv guarantee

--- UDPClient.cc
+++ UDPClient.cc
@@ -1,51 +1,93 @@
-// Client side implementation of UDP client-server model
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+// client code for UDP socket programming
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define PORT     8080
-#define MAXLINE 1024
-#define MSG_CONFIRM 0
+#define IP_PROTOCOL 0
+#define IP_ADDRESS "127.0.0.1" // localhost
+#define PORT_NO 15050
+#define NET_BUF_SIZE 32
+#define cipherKey 'S'
+#define sendrecvflag 0
 
-// Driver code
-int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from client";
-    struct sockaddr_in     servaddr;
+// funtion to clear buffer
+void clearBuf(char* b)
+{
+    int i;
+    for (i = 0; i < NET_BUF_SIZE; i++)
+        b[i] = '\0';
+}
 
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+// function for decryption
+char Cipher(char ch)
+{
+    return ch ^ cipherKey;
+}
+
+// function to receive file
+int recvFile(char* buf, int s)
+{
+    int i;
+    char ch;
+    for (i = 0; i < s; i++) {
+        ch = buf[i];
+        ch = Cipher(ch);
+        if (ch == EOF)
+            return 1;
+        else
+            printf("%c", ch);
     }
+    return 0;
+}
 
-    memset(&servaddr, 0, sizeof(servaddr));
+// driver code
+int main()
+{
+    int sockfd, nBytes;
+    struct sockaddr_in addr_con;
+    socklen_t addrlen = sizeof(addr_con);
+    addr_con.sin_family = AF_INET;
+    addr_con.sin_port = htons(PORT_NO);
+    addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    char net_buf[NET_BUF_SIZE];
+    FILE* fp;
 
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    // socket()
+    sockfd = socket(AF_INET, SOCK_DGRAM,
+                    IP_PROTOCOL);
 
-    int n, len;
-    socklen_t sendsize = sizeof(servaddr);
+    if (sockfd < 0)
+        printf("\nfile descriptor not received!!\n");
+    else
+        printf("\nfile descriptor %d received\n", sockfd);
 
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
+    while (1) {
+        printf("\nPlease enter file name to receive:\n");
+        scanf("%s", net_buf);
+        sendto(sockfd, net_buf, NET_BUF_SIZE,
+               sendrecvflag, (struct sockaddr*)&addr_con,
+               addrlen);
 
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &sendsize);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+        printf("\n---------Data Received---------\n");
 
-    close(sockfd);
+        while (1) {
+            // receive
+            clearBuf(net_buf);
+            nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE,
+                              sendrecvflag, (struct sockaddr*)&addr_con,
+                              &addrlen);
+
+            // process
+            if (recvFile(net_buf, NET_BUF_SIZE)) {
+                break;
+            }
+        }
+        printf("\n-------------------------------\n");
+    }
     return 0;
 }
